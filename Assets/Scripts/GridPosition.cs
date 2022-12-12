d24be52700d458a6d9d02e477098891790f52b36
Fenneig <ninetailedkot92@gmail.com>
2022-12-12 09:19:01 +0300

    implement grid system

---
+++ Assets/Scripts/GridPosition.cs
@@ -0,0 +1,16 @@
+﻿public struct GridPosition
+{
+    public int X;
+    public int Z;
+
+    public GridPosition(int x, int z)
+    {
+        X = x;
+        Z = z;
+    }
+
+    public override string ToString()
+    {
+        return $"X : {X}; Z : {Z}";
+    }
+}
