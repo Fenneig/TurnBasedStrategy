6943dec89686011a412ab9cb245cb7fb43bd601f
Fenneig <ninetailedkot92@gmail.com>
2022-12-30 06:19:03 +0300

    add crate explosion effect

--- Assets/Scripts/Utils/DestructibleCrate.cs
+++ Assets/Scripts/Utils/DestructibleCrate.cs
@@ -1,12 +1,16 @@
 using System;
 using Grid;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Utils
 {
     public class DestructibleCrate : MonoBehaviour, IDamageable
     {
         public static event EventHandler OnAnyDestroyed;
+
+        [SerializeField] private Transform[] _crateDestroyedPrefabs;
 
         public GridPosition GridPosition { get; private set; }
 
@@ -17,8 +21,27 @@ namespace Utils
 
         public void Damage(int _, Vector3 incomeDamagePosition)
         {
-            OnAnyDestroyed?.Invoke(this,EventArgs.Empty);
+            OnAnyDestroyed?.Invoke(this, EventArgs.Empty);
+            int randomPrefabIndex = Random.Range(0, _crateDestroyedPrefabs.Length);
+            Transform crateDestroyedTransform = Instantiate(_crateDestroyedPrefabs[randomPrefabIndex],
+                transform.position, quaternion.identity);
+            Vector3 damageImpulse = (incomeDamagePosition - transform.position).normalized;
+            ApplyExplosionToChildren(crateDestroyedTransform, 150f, transform.position + damageImpulse, 10f);
             Destroy(gameObject);
+        }
+
+        private void ApplyExplosionToChildren(Transform root, float explosionForce, Vector3 explosionPosition,
+            float explosionRange)
+        {
+            foreach (Transform child in root)
+            {
+                if (child.TryGetComponent(out Rigidbody childRigidbody))
+                {
+                    childRigidbody.AddExplosionForce(explosionForce, explosionPosition, explosionRange);
+                }
+
+                ApplyExplosionToChildren(child, explosionForce, explosionPosition, explosionRange);
+            }
         }
     }
 }
